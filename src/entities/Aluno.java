4f46bfe493a2f145fb1a844f5bcf7b98a4d5df02
Luiz Filipe <luizfilipemr17@gmail.com>
2023-04-25 18:09:11 -0300

    Criando a classe aluno

--- src/entities/Aluno.java
+++ src/entities/Aluno.java
@@ -1,5 +1,31 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
+    // Tem que ter 8 dígitos
+    private Long rgm;
+    private String nome;
+    private List<String> materias;
+
+    public Aluno(Long rgm, String nome) {
+        this.rgm = rgm;
+        this.nome = nome;
+        this.materias = new ArrayList<>();
+    }
+
+    public Long getRgm() {
+        return rgm;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<String> getMaterias() {
+        return materias;
+    }
+    
     
 }
