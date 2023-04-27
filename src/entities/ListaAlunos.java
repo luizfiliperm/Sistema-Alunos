499d57823b9d1739e68e172b1712401a16f07d57
João pedro soares Figueiredo <jpsoares14@hotmail.com>
2023-04-27 13:47:09 -0300

    removeraluno

--- src/entities/ListaAlunos.java
+++ src/entities/ListaAlunos.java
@@ -1,7 +1,5 @@
 package entities;
 
-
-
 public class ListaAlunos {
 
     public final int TAM = 60;
@@ -12,37 +10,42 @@ public class ListaAlunos {
         listaAlunos = new Aluno[TAM];
         tamAtual = 0;
     }
-    
+
     // método de cadastro de aluno
-    public void cadastrarAluno(String nome, Long rgm){
-    
+    public void cadastrarAluno(String nome, Long rgm) {
         int pos = 0;
-       while(pos<tamAtual && listaAlunos[pos].getRgm() < rgm){
-        pos++;
-       }
-       for(int i=tamAtual;i>pos;i--){
-        listaAlunos[i]=listaAlunos[i-1];
-       }
-
-       listaAlunos[pos]= new Aluno(rgm, nome);
-       tamAtual++;
-
+        while (pos < tamAtual && listaAlunos[pos].getRgm() < rgm) {
+            pos++;
+        }
+        for (int i = tamAtual; i > pos; i--) {
+            listaAlunos[i] = listaAlunos[i - 1];
+        }
+        listaAlunos[pos] = new Aluno(rgm, nome);
+        tamAtual++;
     }
-    
-    public boolean AlunoExiste(Long rgm){
 
-        for(int i=0;i<tamAtual-1;i++){
-            if(rgm==listaAlunos[i].getRgm()){
+    public boolean alunoExiste(Long rgm) {
+        for (int i = 0; i < tamAtual; i++) {
+            if (rgm == listaAlunos[i].getRgm()) {
                 return true;
             }
         }
         return false;
     }
 
-
-    public void removerAluno(Long rgm) {
+    public void removerAlunoPorRgm(Long rgm) {
+        for (int i = 0; i < tamAtual; i++) {
+            if (listaAlunos[i] != null && listaAlunos[i].getRgm().equals(rgm)) {
+                for (int j = i; j < tamAtual - 1; j++) {
+                    listaAlunos[j] = listaAlunos[j + 1];
+                }
+                listaAlunos[tamAtual - 1] = null;
+                tamAtual--;
+                break;
+            }
+        }
     }
-
 }
+
 
        
