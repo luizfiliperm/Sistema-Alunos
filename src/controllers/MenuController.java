116b4030d922a0c60e99e221b5ae5ba3cf3cb55f
Luiz Filipe <luizfilipemr17@gmail.com>
2023-04-29 11:52:48 -0300

    Controlador da classe menu pronto

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -1,10 +1,14 @@
 package controllers;
 
+import java.io.IOException;
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class MenuController {
+    ScreenMethods sm = new ScreenMethods();
 
     @FXML
     private Button btCadastrarAluno;
@@ -19,23 +23,23 @@ public class MenuController {
     private Button btSair;
 
     @FXML
-    void ListaAlunos(ActionEvent event) {
-        
+    void ListaAlunos(ActionEvent event) throws IOException{
+        sm.changeScreen("ListaDeAlunos", event);
     }
 
     @FXML
-    void cadastrarAluno(ActionEvent event) {
-
+    void cadastrarAluno(ActionEvent event) throws IOException{
+        sm.changeScreen("CadastrarAluno", event);
     }
 
     @FXML
-    void cadastrarMateria(ActionEvent event) {
-
+    void cadastrarMateria(ActionEvent event) throws IOException {
+        sm.changeScreen("CadastrarMateria", event);
     }
 
     @FXML
     void sair(ActionEvent event) {
-
+        Platform.exit();
     }
 
 }
