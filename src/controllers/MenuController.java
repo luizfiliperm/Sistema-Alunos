243ca6fd27fc148df055c0dd7bdac6ed2e204064
Luiz Filipe <luizfilipemr17@gmail.com>
2023-04-29 11:37:59 -0300

    Adcionando Controlador do menu

---
+++ src/controllers/MenuController.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    private Button btCadastrarAluno;
+
+    @FXML
+    private Button btCadastrarMateria;
+
+    @FXML
+    private Button btListaAlunos;
+
+    @FXML
+    private Button btSair;
+
+    @FXML
+    void ListaAlunos(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void cadastrarAluno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cadastrarMateria(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sair(ActionEvent event) {
+
+    }
+
+}
