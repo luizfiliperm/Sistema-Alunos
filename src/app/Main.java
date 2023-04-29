17b9742acc772ade7825b8839e520fce0cd6f9a3
Luiz Filipe <luizfilipemr17@gmail.com>
2023-04-29 11:29:22 -0300

    Adcionando a primeira tela

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,34 @@
 package app;
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        String mainPath = "../views/Menu.fxml";
+        Parent root = FXMLLoader.load(getClass().getResource(mainPath));
+        Scene scene = new Scene(root);
+
+        // Adcionar o ícone depois
+        // Image icon = new Image(getClass().getResourceAsStream("path")); 
+        // primaryStage.getIcons().add(icon);
+
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Colegio Corleone");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        
     }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    
 }
